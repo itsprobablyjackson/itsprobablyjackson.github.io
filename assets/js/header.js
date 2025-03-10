7ea42984e40c732d81ae7414f1d5f4f55f23ad71
phillipdelphias <phillipdelphias@gmail.com>
2025-03-10 17:30:04 +1100

    more script fixes

--- assets/js/header.js
+++ assets/js/header.js
@@ -3,7 +3,7 @@ const preloadedHeader = document.querySelector('.mainheader')
 async function loadHeader() {
     console.log('no header')
     
-    const r = await fetch('./assets/preloadhtml/header.html')
+    const r = await fetch('/assets/preloadhtml/header.html')
 
     const header = await r.text()
 
