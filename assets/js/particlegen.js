17cf641e22ab4007e0e5efb0906151a743356c3f
phillipdelphias <phillipdelphias@gmail.com>
2025-03-10 17:32:08 +1100

    global particle fix

--- assets/js/particlegen.js
+++ assets/js/particlegen.js
@@ -1,4 +1,4 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', './assets/css/particles.json', function() {
+particlesJS.load('particles-js', '/assets/css/particles.json', function() {
   console.log('callback - particles.js config loaded');
 });
